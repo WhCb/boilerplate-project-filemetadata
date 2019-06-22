4f54e307c55170e3a2aa7d08da57b9a33c40e821
Suat Sayharman <suat.sayharman@gmail.com>
2019-06-22 10:29:35 +0300

    File Detail Endpoint is added
    Multer powered middleware is added

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+// Uploaded File Details Endpoint
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  const { file: { originalname, mimetype, size } } = req;
+  
+  res.json({ name: originalname, type: mimetype, size })
+  
+  next();
+})
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
